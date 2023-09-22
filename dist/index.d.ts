f64fd7d91b66d014627e2aeb02266254e5272233
Izzat Haikal <izzat7641@gmail.com>
2023-09-22 14:34:07 +0800

    refine declaration

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,46 @@
+// Type for ReactPageTastic 1.0.0
+
 import React = require("react");
 
 export function ReactPageTastic({ onPageChange, totalCount, siblingCount, currentPage, pageSize, classNamePrefix, leftArrow, rightArrow, }: {
+    /**
+     * Function that receives the page number when clicked.
+     */
     onPageChange: (page: number) => void;
+
+    /**
+     * The total number of pages.
+     */
     totalCount: number;
+
+    /**
+     * The number of pages to display for sibling pages (previous and next to the current page).
+     */
     siblingCount: number;
+
+    /**
+     * The current page number.
+     */
     currentPage: number;
+
+    /**
+     * The number of items per page.
+     */
     pageSize: number;
+
+    /**
+     * The prefix for the class name.
+     */
     classNamePrefix: string;
+
+    /**
+     * The left arrow element.
+     */
     leftArrow: React.JSX.Element;
+
+    /**
+     * The right arrow element.
+     */
     rightArrow: React.JSX.Element;
+
 }): import("react").JSX.Element;
